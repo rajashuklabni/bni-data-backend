a08e9860e62d96336c4ad5cfd1c83ebf9a4b289d
Prince Sachdeva <scriptforprince@gmail.com>
2024-10-09 17:26:58 +0530

    Added all links in the cors

--- connect.js
+++ connect.js
@@ -19,8 +19,9 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5500/',
     'http://127.0.0.1:5500',
-    'https://bni-dashboard-backend.vercel.app/',
-    'https://bni-dashboard-backend.vercel.app', // Add more allowed origins as needed
+    'https://bni-dashboard-backend.vercel.app/*',
+    'https://bni-dashboard-backend.vercel.app',
+    '*', // Add more allowed origins as needed
 ];
 
 const corsOptions = {
