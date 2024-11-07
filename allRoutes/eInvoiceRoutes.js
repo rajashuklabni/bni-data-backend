c0c189cbea05693df4b2ba565e32e065ea0b0c73
Prince Sachdeva <scriptforprince@gmail.com>
2024-11-07 18:45:15 +0530

    Generated IRN

--- allRoutes/eInvoiceRoutes.js
+++ allRoutes/eInvoiceRoutes.js
@@ -1,9 +1,10 @@
 // allRoutes/elvoiceRoutes.js
 const express = require("express");
 const router = express.Router();
-const { getToken } = require("../allControllers/paymentControllers/elvoiceController");
+const { getToken, generateIRN } = require("../allControllers/paymentControllers/elvoiceController");
 
 // POST endpoint to generate an e-invoice
 router.post("/get-token", getToken);
+router.post("/generate-irn", generateIRN);
 
 module.exports = router;
