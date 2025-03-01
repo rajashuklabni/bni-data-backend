e26cf35453776971e6d9a30daa16fb405b875bfe
Prince Sachdeva <scriptforprince@gmail.com>
2025-03-01 15:51:23 +0530

    Get gst details fetched correctly

--- allRoutes/eInvoiceRoutes.js
+++ allRoutes/eInvoiceRoutes.js
@@ -1,11 +1,19 @@
 // allRoutes/elvoiceRoutes.js
 const express = require("express");
 const router = express.Router();
-const { getToken, generateIRN } = require("../allControllers/paymentControllers/elvoiceController");
+const { getToken, generateIRN, cancelIRN, getGstDetails  } = require("../allControllers/paymentControllers/elvoiceController");
 
 // POST endpoint to generate an e-invoice
 router.post("/get-token", getToken);
 router.post('/generate-irn', async (req, res) => {
     await generateIRN(req, res);
   });
+router.post("/cancel-irn", async (req, res) => {
+    await cancelIRN(req, res);
+});
+
+// New Route for Fetching GST Details
+router.get("/get-gst-details/:gstNo", async (req, res) => {
+  await getGstDetails(req, res);
+});
 module.exports = router;
