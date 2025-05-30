c9f9bef116661bbe34d3e2a26d8d486801cde08a
scriptforprince7 <scriptforprince@gmail.com>
2025-05-30 11:38:09 +0530

    Some changes in cashfree webhook controller

--- allRoutes/paymentRoute.js
+++ allRoutes/paymentRoute.js
@@ -4,23 +4,6 @@ const router = express.Router();
 const app = express();
 const { sessionIdGenerator,getOrderStatus,getPaymentStatus, getSettlementStatus, getSettlementByCfPaymentId, getOrderByTrainingId, webhookSettlementStatus} = require('../allControllers/paymentControllers/cashfreeSessionIdController');
 
-// Disable body parsing for the webhook route
-router.use('/webhook/settlementStatus', (req, res, next) => {
-  if (req.method === 'POST') {
-    let data = '';
-    req.setEncoding('utf8');
-    req.on('data', chunk => {
-      data += chunk;
-    });
-    req.on('end', () => {
-      req.rawBody = data;
-      next();
-    });
-  } else {
-    next();
-  }
-});
-
 router.post('/generate-cashfree-session',sessionIdGenerator);
 router.get('/getCashfreeOrderDataAndVerifyPayment/:order_id',getOrderStatus)
 router.get('/orders/:order_id/paymentStatus',getPaymentStatus)
@@ -28,11 +11,13 @@ router.get('/orders/:order_id/settlementStatus', getSettlementStatus);
 router.get('/settlement/:cf_payment_id', getSettlementByCfPaymentId);
 router.get('/getTrainingOrder/:training_id', getOrderByTrainingId);
 
-// Configure the webhook route
+// Configure the webhook route to handle raw bodies
 router.post('/webhook/settlementStatus', 
+  bodyParser.raw({ type: 'application/json' }), 
   (req, res, next) => {
     console.log('Webhook middleware - Content-Type:', req.headers['content-type']);
-    console.log('Webhook middleware - Raw body:', req.rawBody);
+    console.log('Webhook middleware - Body type:', typeof req.body);
+    console.log('Webhook middleware - Is Buffer:', Buffer.isBuffer(req.body));
     next();
   },
   webhookSettlementStatus
